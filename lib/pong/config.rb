83ed25f50cd6fc0e48d1ec19c3734b60111628b6
Alex Klyanchin <jetblackmeerkat@gmail.com>
2019-01-07 20:15:23 +0300

    Fix ambiguous arguments

--- lib/pong/config.rb
+++ lib/pong/config.rb
@@ -10,7 +10,7 @@ module Pong
       Logger = Types::Any.constrained(type: Logger)
     end
 
-    attribute :logger, Types::Logger.default { ::Logger.new(STDOUT) }
+    attribute(:logger, Types::Logger.default { ::Logger.new(STDOUT) })
     attribute :window_width, Types::Strict::Integer
     attribute :window_height, Types::Integer
     attribute :window_caption, Types::String.optional.default('Pong')
