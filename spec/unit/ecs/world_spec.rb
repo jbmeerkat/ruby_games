e90218ae1efcf01ae54f71e2b6991c00a9423498
Alex Klyanchin <jetblackmeerkat@gmail.com>
2019-01-08 01:30:54 +0300

    Slightly improve ECS::World test coverage

--- spec/unit/ecs/world_spec.rb
+++ spec/unit/ecs/world_spec.rb
@@ -3,24 +3,47 @@
 require 'ecs_helper'
 
 RSpec.describe ECS::World do
-  let(:test_system) do
-    Class.new(ECS::System) do
-      run_on :update
-    end.new
-  end
-
   let(:world) { described_class.new(width: rand(10), height: rand(10)) }
 
-  before do
-    allow(test_system).to receive(:run)
+  describe '#update' do
+    let(:test_system) do
+      Class.new(ECS::System) do
+        run_on :update
+      end.new
+    end
 
-    world.add_system(test_system)
+    before do
+      allow(test_system).to receive(:run)
 
-    world.update(time_delta: 5)
-    world.update(time_delta: 5)
+      world.add_system(test_system)
+
+      world.update(time_delta: 5)
+      world.update(time_delta: 5)
+    end
+
+    it 'runs all the update systems' do
+      expect(test_system).to have_received(:run).twice
+    end
   end
 
-  it '#update' do
-    expect(test_system).to have_received(:run).twice
+  describe '#draw' do
+    let(:test_system) do
+      Class.new(ECS::System) do
+        run_on :draw
+      end.new
+    end
+
+    before do
+      allow(test_system).to receive(:run)
+
+      world.add_system(test_system)
+
+      world.time_delta = 0
+      world.draw
+    end
+
+    it 'runs all the draw systems' do
+      expect(test_system).to have_received(:run)
+    end
   end
 end
