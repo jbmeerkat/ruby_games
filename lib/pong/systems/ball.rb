8aec71d46424071949da97f51e2bd3b6c8ab43ca
Alex Klyanchin <jetblackmeerkat@gmail.com>
2019-01-09 00:38:48 +0300

    Add platforms collision

--- lib/pong/systems/ball.rb
+++ lib/pong/systems/ball.rb
@@ -6,22 +6,50 @@ module Pong
     class Ball < ECS::System
       run_on :update
 
-      def run
-        registry = world.entity_registry
-        ball = registry.entities_by_tag[:ball].first
-        velocity = registry.entity_component(ball, :velocity)
-        return unless collides_with_horizontal_bounds?(ball)
+      def_delegator :world, :entity_registry
 
-        velocity.y = -velocity.y
+      def run
+        ball = entity_registry.entities_by_tag[:ball].first
+        velocity = entity_registry.entity_component(ball, :velocity)
+
+        if collides_with_horizontal_bounds?
+          velocity.y = -velocity.y
+        elsif collides_with_platform?
+          velocity.x = -velocity.x
+        end
       end
 
       private
 
-      def collides_with_horizontal_bounds?(ball)
-        registry = world.entity_registry
-        rectangle = registry.entity_component(ball, :rectangle)
+      def collides_with_horizontal_bounds?
+        rectangle = rectangle_by_tag(:ball)
 
         rectangle.y + rectangle.height > world.height || rectangle.y.negative?
+      end
+
+      def collides_with_platform?
+        ball = rectangle_by_tag(:ball)
+        left_platform = rectangle_by_tag(:left_platform)
+        right_platform = rectangle_by_tag(:right_platform)
+
+        rectangle_collision?(ball, left_platform) ||
+          rectangle_collision?(ball, right_platform)
+      end
+
+      def rectangle_collision?(first, second)
+        first.x < second.x + second.width &&
+          first.x + first.width > second.x &&
+          first.y < second.y + second.height &&
+          first.y + first.height > second.y
+      end
+
+      def rectangle_by_tag(tag)
+        entity = entity_registry.entities_by_tag[tag].first
+        entity_rectangle(entity)
+      end
+
+      def entity_rectangle(entity)
+        entity_registry.entity_component(entity, :rectangle)
       end
     end
   end
--- spec/integration/pong/systems/ball_spec.rb
+++ spec/integration/pong/systems/ball_spec.rb
@@ -4,40 +4,70 @@ require 'pong_helper'
 
 RSpec.describe Pong::Systems::Ball do
   let(:system) { described_class.new }
+  let(:world) { ECS::World.new(width: 10, height: 10) }
 
   before do
-    world = ECS::World.new(width: 10, height: 10)
     system.world = world
     ball = world.create_entity(:ball)
 
-    world.entity_registry.add_component(ball, velocity)
-
+    world.entity_registry.add_component(ball, ball_velocity)
     world.entity_registry.add_component(
       ball,
-      Pong::Components::Rectangle[position: position, width: 10, height: 10]
+      Pong::Components::Rectangle[position: ball_position, width: 5, height: 5]
+    )
+
+    left_platform = world.create_entity(:left_platform)
+    world.entity_registry.add_component(
+      left_platform,
+      Pong::Components::Rectangle[
+        position: Pong::Components::Position[x: 0, y: 0],
+        width: 10,
+        height: 10
+      ]
+    )
+
+    right_platform = world.create_entity(:right_platform)
+    world.entity_registry.add_component(
+      right_platform,
+      Pong::Components::Rectangle[
+        position: Pong::Components::Position[x: 0, y: 100],
+        width: 10,
+        height: 10
+      ]
     )
   end
 
   describe '#run' do
     context "when ball collides with world's lower bound" do
-      let(:velocity) { Pong::Components::Velocity[x: 0, y: 10] }
-      let(:position) { Pong::Components::Position[x: 0, y: 5] }
+      let(:ball_velocity) { Pong::Components::Velocity[x: 0, y: 10] }
+      let(:ball_position) { Pong::Components::Position[x: 0, y: world.height] }
 
       it 'ball bounces in opposite direction' do
         expect { system.run }.to change(
-          velocity, :y
-        ).from(10).to(-10).and not_change(velocity, :x)
+          ball_velocity, :y
+        ).from(10).to(-10).and not_change(ball_velocity, :x)
       end
     end
 
     context "when ball collides with world's upper bound" do
-      let(:velocity) { Pong::Components::Velocity[x: 0, y: -10] }
-      let(:position) { Pong::Components::Position[x: 0, y: -5] }
+      let(:ball_velocity) { Pong::Components::Velocity[x: 0, y: -10] }
+      let(:ball_position) { Pong::Components::Position[x: 0, y: -5] }
 
       it 'ball bounces in opposite direction' do
         expect { system.run }.to change(
-          velocity, :y
-        ).from(-10).to(10).and not_change(velocity, :x)
+          ball_velocity, :y
+        ).from(-10).to(10).and not_change(ball_velocity, :x)
+      end
+    end
+
+    context 'when ball collides with platform' do
+      let(:ball_velocity) { Pong::Components::Velocity[x: -10, y: 0] }
+      let(:ball_position) { Pong::Components::Position[x: 5, y: 5] }
+
+      it 'ball bounces in opposite direction' do
+        expect { system.run }.to change(
+          ball_velocity, :x
+        ).from(-10).to(10).and not_change(ball_velocity, :y)
       end
     end
   end
